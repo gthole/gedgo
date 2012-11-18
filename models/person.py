532ddd40014d8735233a12ce6406caff0e03dd2f
gthole <gthole@gmail.com>
2012-11-18 12:42:44 -0500

    Keyword filtering for automatic profile pic selection.

--- models/person.py
+++ models/person.py
@@ -4,6 +4,8 @@ from django.conf import settings
 
 from document import Document
 
+from re import findall
+
 class Person(models.Model):
 	class Meta:
 		app_label = 'gedgo'
@@ -50,8 +52,13 @@ class Person(models.Model):
 			return self.profile.all()[0]
 		
 		photos = Document.objects.filter(tagged_people=self, kind='PHOTO')
+		
 		if photos:
-			return photos[len(photos)-1]
+			name_filtered = filter(lambda p: len(findall(BLOCK_TAGS, p.docfile.name))==0, photos)
+			if name_filtered:
+				return name_filtered[len(name_filtered)-1]
+			else:
+				return photos[len(photos)-1]
 	
 	def __unicode__(self):
 		return self.last_name + ', ' + self.first_name + ' (' + self.pointer + ')'
@@ -76,3 +83,8 @@ class Person(models.Model):
 			return '\n'.join(self.education.strip(';').split(';'))
 		else:
 			return ''
+
+# Keywords to filter out document-type photos in preference of portraits.
+BLOCK_TAGS = ("(?i)(?:death|birth|masscard|census|burial|tax|obit|cemet(?:a|e)ry|" +
+			  "(?:grave|head)stone|baptism|baptcert|baptrec|burrec|lists?\\b|military|" +
+			  "record|letter|\\bwill\\b|hsdip|road|street|directory)")

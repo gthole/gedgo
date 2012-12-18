410d0f520d838c0fc8da1fcced4ce42ba2032d85
Greg Thole <no@not.net>
2012-12-17 20:29:33 -0500

    Some more adjustments to gedcom update code.

--- tasks.py
+++ tasks.py
@@ -13,15 +13,18 @@ from os import remove
 @task(name='gedgo.tasks.async_update')
 def async_update(gedcom, target):
 	start = datetime.now()
-	update(gedcom, target)
+	errstr = ''
+	try:
+		update(gedcom, target)
+	except Exception as e:
+		errstr = 'There was an error!\n' + e.strerror
+	remove(target)  # Delete temporary file.
 	end = datetime.now()
 	send_mail(
 			'Update finished!',
 			'Started:  ' + start.strftime('%B %d, %Y at %I:%M %p') + '\n' +
-			'Finished: ' + end.strftime('%B %d, %Y at %I:%M %p'),
+			'Finished: ' + end.strftime('%B %d, %Y at %I:%M %p') + '\n\n' +
+			errstr,
 			'noreply@example.com',
 			[settings.SERVER_EMAIL],
 		)
-
-	# Delete temp file.
-	remove(target)
--- views/update.py
+++ views/update.py
@@ -4,7 +4,7 @@ from gedgo.tasks import async_update
 
 from os.path import isfile
 
-from django.shortcuts import get_object_or_404, render_to_response, redirect
+from django.shortcuts import get_object_or_404, render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.conf import settings
@@ -14,16 +14,20 @@ from django.conf import settings
 def update_view(request, gedcom_id):
 	g = get_object_or_404(Gedcom, id=gedcom_id)
 
-	if request.method == 'POST':
+	tmpname = settings.MEDIA_ROOT + 'documents/tmp.ged'
+	message = ''
+
+	if isfile(tmpname):
+		form = ''
+		message = 'This gedcom is currently being updated already.  `Please check back later.'
+	elif request.method == 'POST':
 		form = UpdateForm(request.POST, request.FILES)
 		# check for temp file
 		if form.is_valid():
 
-			# Need to distribute-task this
-			tmpname = settings.MEDIA_ROOT + 'documents/tmp.ged'
-			if isfile(tmpname):
-				return redirect('/gedgo/')  # Needs to return a "wait" message.
-
+			# TODO: Replace tmp file with a database value setting or dj-celery check,
+			# and skip open/write/close cycle, feeding document value in directly.
+			# Should have validation of file name, file contents before proceeding.
 			tmp = open(tmpname, 'w+')
 			tmp.write(request.FILES['gedcom_file'].read())
 			tmp.close()
@@ -31,9 +35,11 @@ def update_view(request, gedcom_id):
 			async_update.delay(g, tmpname)
 
 			# Redirect to the document list after POST
-			return redirect('/gedgo/' + str(g.id))  # Needs to return a success message.
+			form = ''
+			message = 'Upload successful.  Gedcom updating.'
 		else:
-			return redirect('/gedgo/')  # Needs to return an error message.
+			form = UpdateForm()
+			message = 'Did you correctly upload a gedcom file?'
 
 	else:
 		form = UpdateForm()
@@ -41,5 +47,5 @@ def update_view(request, gedcom_id):
 	# Render list page with the documents and the form
 	return render_to_response(
 		'gedgo/update.html',
-		{'form': form, 'gedcom': g},
+		{'form': form, 'message': message, 'gedcom': g},
 		context_instance=RequestContext(request))

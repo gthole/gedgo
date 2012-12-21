c1accb2aa0f176d9840e091872d8ed1bc3d52e1d
Greg Thole <no@not.net>
2012-12-21 10:45:48 -0500

    Limit to GET requests and add photos references.

--- api.py
+++ api.py
@@ -3,6 +3,15 @@ from gedgo.models import Person
 
 
 class PersonResource(ModelResource):
-    class Meta:
-        queryset = Person.objects.all()
-        resource_name = 'person'
+	class Meta:
+		list_allowed_methods = ['get']
+		queryset = Person.objects.all()
+		resource_name = 'person'
+
+	def dehydrate(self, bundle):
+		photos = bundle.obj.photos()
+		if not photos:
+			return bundle
+		images = map(lambda i: i.docfile.url, photos)
+		bundle.data['images'] = images
+		return bundle

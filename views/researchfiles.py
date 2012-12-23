e9adb5e3a68d510e15217d172b0721ba09c06abf
Greg Thole <no@not.net>
2012-12-23 13:37:31 -0500

    Nice-ify "Research Files" module.

--- views/researchfiles.py
+++ views/researchfiles.py
@@ -22,10 +22,24 @@ def researchfiles(request, pathname):
 		return serve_content(r)
 	elif path.isdir(r):
 		dir_contents = filter(lambda c: not c[0] == '.', listdir(r))
+		dir_contents = map(lambda c: (path.isdir(path.join(r, c)), c), dir_contents)
+
 		if (len(pathname) > 0) and (not pathname[0] == '/'):
 			pathname = '/' + pathname
+		pathname = path.normpath(pathname)
+		if pathname == '.':
+			pathname = ''
+
+		levels = pathname.split('/')  # Is there a better way than this?
+
+		if len(pathname) < 2:
+			current_level = 'Research Files'
+		else:
+			current_level = levels[-1]
+
 		return render_to_response('gedgo/researchfiles.html',
-			{'contents': dir_contents, 'path': pathname},
+			{'contents': dir_contents, 'levels': levels, 'path': pathname,
+			'current_level': current_level},
 			context_instance=RequestContext(request, site_context(request)))
 	else:
 		raise Http404
